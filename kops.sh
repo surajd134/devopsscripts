451eae9315a4824e4cae9ea7ffb7eba215b6f0b2
Suraj Divate <119672864+surajd134@users.noreply.github.com>
2025-07-22 12:19:26 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -10,10 +10,9 @@ wget https://github.com/kubernetes/kops/releases/download/v1.32.0/kops-linux-amd
 chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
-aws s3api create-bucket --bucket reyaz-kops-testbkt143.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
-aws s3api put-bucket-versioning --bucket reyaz-kops-testbkt143.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://reyaz-kops-testbkt143.k8s.local
-kops create cluster --name reyaz.k8s.local --zones ap-south-1a --image ami-0f918f7e67a3323f0  --control-plane-count=1 --control-plane-size t2.medium --node-count=2 --node-size t2.micro
-kops update cluster --name reyaz.k8s.local --yes --admin
-
+aws s3api create-bucket --bucket suraj-kops-testbkt143.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
+aws s3api put-bucket-versioning --bucket suraj-kops-testbkt143.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://suraj-kops-testbkt143.k8s.local
+kops create cluster --name suraj.k8s.local --zones ap-south-1a --image ami-0f918f7e67a3323f0  --control-plane-count=1 --control-plane-size t2.medium --node-count=2 --node-size t2.medium
+kops update cluster --name suraj.k8s.local --yes --admin
 #kops validate cluster --wait 10m
